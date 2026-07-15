a6f2823ee539d2141726e29c0fdb5eef45c3ebc4
Jeevika V <vpjeevika@gmail.com>
2026-07-15 16:16:54 +0530

    Create 003_percentage.java

---
+++ Basic Java/003_percentage.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int max_score=500;
+        int userscore=421;
+        double percentage= (double)userscore / max_score*100.d;
+        System.out.println(percentage);
+    }
+}
