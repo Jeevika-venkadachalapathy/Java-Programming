22acea1039861df085df82aae4fb6b2049b87b71
Jeevika V <vpjeevika@gmail.com>
2026-07-15 16:16:11 +0530

    Create 002_type_casting.java

---
+++ Basic Java/002_type_casting.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        double myDouble=29.0d;
+        int myInt= (int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
