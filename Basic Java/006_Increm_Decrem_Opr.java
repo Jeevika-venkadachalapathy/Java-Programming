d37fe25c1209c4e7d241c76444ee05b3ad012f8d
Jeevika V <vpjeevika@gmail.com>
2026-07-16 18:55:08 +0530

    Create 006_Increm_Decrem_Opr.java

---
+++ Basic Java/006_Increm_Decrem_Opr.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int a=10;
+        a++;
+        System.out.println(a);
+        
+        int b=20;
+        --b;
+        System.out.println(b);
+        
+        System.out.println("add : "+(a+b));
+        System.out.println("sub : "+(a-b));
+    }
+}
