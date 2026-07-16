298b6404cd36a0deee387f75b0aa2ac17cce312a
Jeevika V <vpjeevika@gmail.com>
2026-07-16 18:35:12 +0530

    Create 004_arithmetic_operators.java

---
+++ Basic Java/004_arithmetic_operators.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+        int x=10;
+        int y=5;
+        
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        
+        int z=6;
+        ++z;
+        System.out.println(z);
+        --z;
+        System.out.println(z);
+    }
+}
