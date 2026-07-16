b63a9071bef6847b6b4b6ba70b22537ff9ed845a
Jeevika V <vpjeevika@gmail.com>
2026-07-16 18:50:01 +0530

    Create 005_CharInt_TypeCasting.java

---
+++ Basic Java/005_CharInt_TypeCasting.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        char myCh= 'A';
+        int myInt= myCh;
+        
+        System.out.println(myInt);
+        
+        int myint= 90;
+        char mych= (char) myint;
+        
+        System.out.println(mych);
+    }
+}
