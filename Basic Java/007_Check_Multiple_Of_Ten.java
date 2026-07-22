79a164eb09265002b961ab2867641514a6f27cd5
Jeevika V <vpjeevika@gmail.com>
2026-07-22 22:19:09 +0530

    Create 007_Check_Multiple_Of_Ten.java

---
+++ Basic Java/007_Check_Multiple_Of_Ten.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int b = s.nextInt();
+		if(n==(10*b))
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+
+	}
+}
