3a62313fd5f099413d58e09a2bf8b9257c993f16
Jeevika V <vpjeevika@gmail.com>
2026-07-24 15:14:50 +0530

    Create 011_ASCII_Value_Finder.java

---
+++ Basic Java/011_ASCII_Value_Finder.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        char ch = s.next().charAt(0);
+        int ascii = (int)ch;
+        System.out.println(ascii);
+    }
+}
