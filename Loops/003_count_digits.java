4a73061876b503a1a495c7bc9e241f290e67d33d
Jeevika V <vpjeevika@gmail.com>
2026-07-30 20:34:04 +0530

    Create 003_count_digits.java

---
+++ Loops/003_count_digits.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        long n = s.nextLong();
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
