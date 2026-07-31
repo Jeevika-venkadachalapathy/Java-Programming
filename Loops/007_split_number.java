aee9d97f92e1cdab8fd172fb6df4e85496b5fc16
Jeevika V <vpjeevika@gmail.com>
2026-07-31 10:39:40 +0530

    Delete Loops/007_split_number.java

--- Loops/007_split_number.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        long n = s.nextLong();
-        System.out.println(n%100000+" "+n/100000);
-    }
-}
