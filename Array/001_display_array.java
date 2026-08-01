7446692ef2a69035f1865d01dd678f3191f4056b
Jeevika V <vpjeevika@gmail.com>
2026-08-01 13:06:39 +0530

    Create 001_display_array.java

---
+++ Array/001_display_array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int arr[] = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = s.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
