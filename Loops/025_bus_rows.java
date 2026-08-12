45a89db69df1a30f323a1edf356fd6018115b3b1
Jeevika V <vpjeevika@gmail.com>
2026-08-12 22:19:54 +0530

    Create 025_bus_rows.java

---
+++ Loops/025_bus_rows.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int x = sc.nextInt();
+
+            int row = (x - 1) / m + 1;
+            int front = row;
+            int back = n - row + 1;
+
+            System.out.println(Math.min(front, back));
+
+            t--;
+        }
+    }
+}
